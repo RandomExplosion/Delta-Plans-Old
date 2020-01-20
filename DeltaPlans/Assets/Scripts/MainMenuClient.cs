c942291b4eec0fe3418344e1593eb7c1b9ac0b30
RandomExplosion <benjaminjmcgaw@gmail.com>
2020-01-20 19:19:59 +1100

    Created Project and Made room Create/Join Screen

---
+++ DeltaPlans/Assets/Scripts/MainMenuClient.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MainMenuClient : MonoBehaviour
+{
+
+    public GameObject[] _menuPages;
+    public TMP_InputField _joinCodeInputField;
+    public TMP_InputField _playerCapInputField;
+
+
+    public void ChangeMenuPage(int index)
+    {
+        if (index <= _menuPages.Length)
+        {
+            for (int i = 0; i < _menuPages.Length; i++)
+            {
+                _menuPages[i].SetActive(i == index);
+            }
+        }
+        else
+        {
+            Debug.LogError("You are trying to go to a page that doesn't exist");
+            throw new System.IndexOutOfRangeException();
+        }
+    }
+
+    #region RootMenu
+        public void PlayButtonPressed()
+        {
+
+        }
+
+        public void OptionsButtonPressed()
+        {
+
+        }
+
+        public void QuitButtonPressed()
+        {
+            #if UNITY_EDITOR == false
+                        Application.Quit();
+            #endif
+
+            #if UNITY_EDITOR == true
+                    UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+    #endregion
+
+    #region MultiplayerMenu
+
+    public void ValidatePlayerCap()
+    {
+        int currentValue = System.Convert.ToInt32(_playerCapInputField.text);
+
+        if (currentValue > 20)
+        {
+            _playerCapInputField.SetTextWithoutNotify("20");
+            return;
+        }
+        else if (currentValue < 6)
+        {
+            _playerCapInputField.SetTextWithoutNotify("6");
+            return;
+        }
+    }
+
+    #endregion
+}
---
+++ DeltaPlans/Assets/Scripts/PhotonLobby.cs
@@ -0,0 +1,98 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+    //Static Public
+    public static PhotonLobby lobby; //Singleton Reference to the current lobby
+
+    public Button _playButton;  //Play button (made interactable when client connects to master server)
+    public TMP_InputField _joinCodeInputField;
+    public TextMeshProUGUI _joinCodeWaitingRoomDisplay; //Displays the Join code for the current room in the waiting room
+    public MainMenuClient _uiManager;
+    public TMP_InputField _playerCap;
+    private void Awake()
+    {
+        lobby = this;   //Singleton Initialisation
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();   //Connect to master photon server 
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player client successfully connected to master server.");
+        _playButton.interactable = true;
+    }
+
+    public void CreateGame() //Called when a user attempts to create a new game
+    {
+        string joinCode = GenerateRoomCode();
+        int playerCap = System.Convert.ToInt32(_playerCap.text);
+        RoomOptions roomOptions = new RoomOptions { IsOpen = true, IsVisible = true, MaxPlayers = System.Convert.ToByte(playerCap)};
+        PhotonNetwork.CreateRoom(joinCode, roomOptions);
+    }
+
+    public void JoinGame() //Called when user attempts to join a game using a Join Code
+    {
+        PhotonNetwork.JoinRoom(_joinCodeInputField.text);
+    }
+
+    public void LeaveGame() //Called when a user prematurely leaves the game
+    {
+        Debug.Log("Leaving Room");
+        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.LeaveLobby();
+    }
+
+    public override void OnJoinedRoom() //Called on a successful room join
+    {
+        base.OnJoinedRoom();
+        Debug.Log("Successfully Joined Room: " + PhotonNetwork.CurrentRoom.Name);
+        Debug.Log("Progressing to Waiting Room");
+        _uiManager.ChangeMenuPage(5);
+        _joinCodeWaitingRoomDisplay.SetText("Join Code: " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message) //Called when a user fails to join a room
+    {
+        Debug.Log("Failed to join room, Error code:" + returnCode.ToString());
+        base.OnJoinRoomFailed(returnCode, message);
+    }
+
+    public static string GenerateRoomCode() //Generate a random 4 character code comprised of capital letters
+    {
+        string validCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string tempCode = "";
+
+        for (int i = 0; i < 6; i++)
+        {
+            int characterID = Random.Range(0, validCharacters.Length);  //Pick a character in the string of valid characters
+            tempCode = tempCode + validCharacters[characterID];         //Add it to the code
+        }
+
+        return tempCode;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (PhotonNetwork.InRoom)
+        {
+            PhotonNetwork.LeaveRoom(); 
+        }
+    }
+}
